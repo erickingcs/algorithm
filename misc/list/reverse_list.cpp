f63c18d32a2a3e95ce0b2b0aaa713fcb88e7c879
erickingcs <eric.king.cs@gmail.com>
2014-02-16 12:10:42 +0800

    review
    Signed-off-by: erickingcs <eric.king.cs@gmail.com>

--- misc/list/reverse_list.cpp
+++ misc/list/reverse_list.cpp
@@ -53,6 +53,33 @@ ListNode* reverse2(ListNode *head)
     return newList.next;
 }
 
+// recursive solution
+ListNode* reverse3(ListNode *cur, ListNode *tail)
+{
+    if (!cur) return tail;
+
+    ListNode *remain = cur->next;
+    cur->next = tail;
+    return reverse3(remain, cur);
+}
+ListNode* reverse3_rec(ListNode *head)
+{
+    return reverse3(head, nullptr);
+}
+
+// recursive solution
+void reverse4_rec(ListNode *&t) // &
+{
+    if (!t || !t->next) return;
+
+    ListNode *remain = t->next;
+    reverse4_rec(remain);
+
+    t->next->next = t;
+    t->next = nullptr;
+    t = remain;
+}
+
 void print_list(ListNode *head)
 {
     while (head) {
@@ -71,9 +98,18 @@ int main(int argc, char *argv[])
     list->next->next->next->next = new ListNode(5);
 
     print_list(list);
+
     ListNode *ret = reverse1(list);
     print_list(ret);
-    print_list(reverse2(ret));
+
+    ret = reverse2(ret);
+    print_list(ret);
+
+    ret = reverse3_rec(ret);
+    print_list(ret);
+
+    reverse4_rec(ret);
+    print_list(ret);
 
     return 0;
 }
